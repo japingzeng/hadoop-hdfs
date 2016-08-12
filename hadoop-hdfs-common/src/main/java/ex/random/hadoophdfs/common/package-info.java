85d50bfd15ed6dbb278396671aad9a9a77652aa2
cdzengjiaping <cdzengjiaping@jd.com>
2016-08-12 09:15:55 +0800

    fix

---
+++ hadoop-hdfs-common/src/main/java/ex/random/hadoophdfs/common/package-info.java
@@ -0,0 +1,16 @@
+/** 
+ * @Title: package-info.java
+ * @Package ex.random.hadoophdfs.common
+ * @Description: TODO(这里用一句话描述这个类的作用) 
+ * @author japing 
+ * @date 2016年8月12日 上午9:12:53 
+ *  
+ */
+/** 
+ * @ClassName: package-info 
+ * @Description: TODO(这里用一句话描述这个类的作用) 
+ * @author japing 
+ * @date 2016年8月12日 上午9:12:53 
+ *  
+ */
+package ex.random.hadoophdfs.common;
